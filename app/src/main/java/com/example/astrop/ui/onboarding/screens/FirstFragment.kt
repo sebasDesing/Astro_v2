79218a6e6814c551216ad438edabfd3165058080
Sebas <sebastiansilvan97@gmail.com>
2023-03-21 12:30:54 -0500

    implmentacion del dots indicator

---
+++ app/src/main/java/com/example/astrop/ui/onboarding/screens/FirstFragment.kt
@@ -0,0 +1,23 @@
+package com.example.astrop.ui.onboarding.screens
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.astrop.R
+
+
+class FirstFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+
+}
