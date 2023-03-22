daee1c3468b90ac6cf4beb9a25b5b6d80b5c1cb2
Sebas <sebastiansilvan97@gmail.com>
2023-03-22 15:33:33 -0500

    feat : MainFragment.kt

--- app/src/main/java/com/example/astrop/ui/mainFragment/MainFragment.kt
+++ app/src/main/java/com/example/astrop/ui/mainFragment/MainFragment.kt
@@ -1,28 +1,24 @@
-package com.example.astrop.ui.home
+package com.example.astrop.ui.mainFragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.astrop.databinding.FragmentHomeBinding
+import com.example.astrop.databinding.FragmentMainBinding
 
 
-class HomeFragment : Fragment() {
+class MainFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
     }
 
 
