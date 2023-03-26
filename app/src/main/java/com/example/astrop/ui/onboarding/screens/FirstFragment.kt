81975d2625425ef1ba85bdc3d6620f3a96cba0ee
Sebas <sebastiansilvan97@gmail.com>
2023-03-26 10:38:03 -0500

    feat: color para la barra inferior de viewpager2

--- app/src/main/java/com/example/astrop/ui/onboarding/screens/FirstFragment.kt
+++ app/src/main/java/com/example/astrop/ui/onboarding/screens/FirstFragment.kt
@@ -4,20 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 import com.example.astrop.R
+import com.example.astrop.databinding.FragmentFirstBinding
 
 
 class FirstFragment : Fragment() {
 
-
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.F1.animation = AnimationUtils.loadAnimation(requireContext(), R.anim.from_bottom)
+    }
 
 }
