b0a602f500d975d43a964c441e259378436147ed
Sebas <sebastiansilvan97@gmail.com>
2023-03-28 10:54:20 -0500

    feat : SigUp Google

--- app/src/main/java/com/example/astrop/ui/sigIn/SigInFragment.kt
+++ app/src/main/java/com/example/astrop/ui/sigIn/SigInFragment.kt
@@ -1,13 +1,21 @@
 package com.example.astrop.ui.sigIn
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import com.example.astrop.R
 import com.example.astrop.databinding.FragmentSigInBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
@@ -15,43 +23,59 @@ class SigInFragment : Fragment() {
 
     private var _binding: FragmentSigInBinding? = null
     private val binding get() = _binding!!
+    private val GOOGLE_SIGN_IN = 100
 
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var authState: FirebaseAuth.AuthStateListener
+
+    private val googleSignInLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                if (account != null) {
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                    firebaseAuth.signInWithCredential(credential).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            Log.i("acountGG", "${account.email}")
+                        }
+                    }
+                }
+            } catch (e: ApiException) {
+                Log.i("acountGG", "$e")
+            }
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         _binding = FragmentSigInBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        firebaseAuth =Firebase.auth
-        fakeSigIn()
+
+        firebaseAuth = Firebase.auth
+        binding.googleBtn.setOnClickListener {
+            val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+
+            val googleClient = GoogleSignIn.getClient(requireActivity(), googleConf)
+            googleClient.signOut()
+            googleSignInLauncher.launch(googleClient.signInIntent)
+        }
     }
 
-    private fun fakeSigIn() {
-        firebaseAuth.signInWithEmailAndPassword("santosilvano97@gmail.com","123456789a")
-            .addOnCompleteListener(requireActivity()) { task->
-                if (task.isSuccessful){
-                    val user =firebaseAuth.currentUser
-                    Log.i("fakesing",user?.uid.toString() )
-                }
-            }
-
-        firebaseAuth.createUserWithEmailAndPassword("santosilvano97@gmail.com","123456789a")
-            .addOnCompleteListener (requireActivity()){task->
-                if(task.isSuccessful){
-
-                    Log.i("fakesiup", "CREADA CHAVO" )
-                }else{
-                    Log.i("fakesiup", "NOO CHAVO" )
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
-
 
 }
