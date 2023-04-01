535e8bd5c44690592f601c86e604d071a5b7d8a0
Sebas <sebastiansilvan97@gmail.com>
2023-04-01 11:38:55 -0500

    feat : binding en DailyImageFragment.kt

--- app/src/main/java/com/example/astrop/ui/dailyImage/DailyImageFragment.kt
+++ app/src/main/java/com/example/astrop/ui/dailyImage/DailyImageFragment.kt
@@ -1,20 +1,22 @@
 package com.example.astrop.ui.dailyImage
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.astrop.R
+import androidx.fragment.app.Fragment
+import com.example.astrop.databinding.FragmentDailyImageBinding
 
 class DailyImageFragment : Fragment() {
-
+    private var _binding: FragmentDailyImageBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_daily_image, container, false)
+        _binding = FragmentDailyImageBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
