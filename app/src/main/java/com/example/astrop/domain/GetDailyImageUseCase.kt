a8e7a37a2c860511de35f5d80c1dae37108fcd32
Sebas <sebastiansilvan97@gmail.com>
2023-04-04 14:58:24 -0500

    feat : se valida que la inormacion este al dia, en el caso de uso

--- app/src/main/java/com/example/astrop/domain/GetDailyImageUseCase.kt
+++ app/src/main/java/com/example/astrop/domain/GetDailyImageUseCase.kt
@@ -1,18 +1,24 @@
 package com.example.astrop.domain
 
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
 import com.example.astrop.data.AstroRepository
 import com.example.astrop.data.database.entities.toDB
 import com.example.astrop.domain.model.DailyImage
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
 
 class GetDailyImageUseCase @Inject constructor(
     private val repository
     : AstroRepository
 ) {
+    @RequiresApi(Build.VERSION_CODES.O)
     suspend operator fun invoke(): List<DailyImage> {
-
+        Log.i("frchadehoy", getCurrentDate())
         val dailyImage = repository.getDailyImageFromDB()
-        return if (dailyImage.isNotEmpty()) {
+        return if (dailyImage.isNotEmpty() && dailyImage[0].date ==getCurrentDate()) {
             dailyImage
         } else {
             val apiDailyImage = repository.getDailyImageFromApi()
@@ -23,4 +29,10 @@ class GetDailyImageUseCase @Inject constructor(
 
     }
 
+
+    fun getCurrentDate(): String {
+        val formatDate = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        val currentDate = Calendar.getInstance().time
+        return formatDate.format(currentDate)
+    }
 }
