a02dc20189e7ce12846f53a692c4a97c304ccc79
Sebas <sebastiansilvan97@gmail.com>
2023-06-12 18:53:17 -0500

    feat: binds on GetAstroTypeUseCase

--- app/src/main/java/com/example/astrop/di/UseCaseModule.kt
+++ app/src/main/java/com/example/astrop/di/UseCaseModule.kt
@@ -2,6 +2,8 @@ package com.example.astrop.di
 
 import com.example.astrop.domain.use_case.get_astro_detail.GetAstroDetailUseCase
 import com.example.astrop.domain.use_case.get_astro_detail.GetAstrosDetailUseCaseImpl
+import com.example.astrop.domain.use_case.get_astro_type.GetAstroTypeUseCase
+import com.example.astrop.domain.use_case.get_astro_type.GetAstroTypeUseCaseImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -11,8 +13,12 @@ import dagger.hilt.android.scopes.ViewModelScoped
 @Module
 @InstallIn(ViewModelComponent::class)
 abstract class UseCaseModule {
-
     @Binds
     @ViewModelScoped
     abstract fun bindsGetAstrosDetailUseCase(getAstrosDetailUseCaseImpl: GetAstrosDetailUseCaseImpl): GetAstroDetailUseCase
+
+
+    @Binds
+    @ViewModelScoped
+    abstract fun bindsGetAstroTypeUseCase(getAstroTypeUseCaseImpl: GetAstroTypeUseCaseImpl): GetAstroTypeUseCase
 }
--- app/src/main/java/com/example/astrop/domain/use_case/get_astro_detail/GetAstrosDetailUseCaseImpl.kt
+++ app/src/main/java/com/example/astrop/domain/use_case/get_astro_detail/GetAstrosDetailUseCaseImpl.kt
@@ -1,12 +1,12 @@
 package com.example.astrop.domain.use_case.get_astro_detail
 
-import com.example.astrop.data.AstroRepository
+import com.example.astrop.data.AstroRepositoryImpl
 import com.example.astrop.data.database.entities.toDB
 import com.example.astrop.domain.model.AstroDetail
 import javax.inject.Inject
 
 class GetAstrosDetailUseCaseImpl @Inject constructor(
-    private val repository:AstroRepository
+    private val repository: AstroRepositoryImpl
 )  : GetAstroDetailUseCase {
 
     override suspend operator fun  invoke(): List<AstroDetail> {
