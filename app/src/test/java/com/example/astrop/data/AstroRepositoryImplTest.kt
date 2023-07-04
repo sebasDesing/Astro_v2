c98a2ef88944a4331d29e6cddc0cd129df473b88
sebas <sebastiansilvan97@gmail.com>
2023-07-04 11:38:57 -0500

    feat: test AstroRepository

--- app/src/test/java/com/example/astrop/data/AstroRepositoryImplTest.kt
+++ app/src/test/java/com/example/astrop/data/AstroRepositoryImplTest.kt
@@ -3,20 +3,17 @@ package com.example.astrop.data
 import com.example.astrop.data.database.dao.AstroDetailDao
 import com.example.astrop.data.database.dao.AstroTypeDao
 import com.example.astrop.data.database.dao.DailyImageDao
-import com.example.astrop.data.database.entities.DailyImageEntity
 import com.example.astrop.data.network.AstroService
+import com.example.astrop.domain.model.AstroDetail
+import com.example.astrop.domain.model.AstroType
 import com.example.astrop.domain.model.DailyImage
-import com.google.common.truth.Truth.assertThat
-import io.mockk.coEvery
 import io.mockk.coVerify
-import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
-import org.mockito.junit.MockitoJUnit
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
@@ -46,11 +43,52 @@ class AstroRepositoryImplTest {
         } returns listOf(DailyImageEntity(1, "20/06/2023", "test_body", "urlTest", "test"))
         */
         val result = astroRepositoryImpl.getDailyImageFromDB()
-       // assertThat(result).isEqualTo(expected)
+        // assertThat(result).isEqualTo(expected)
 
-        coVerify (exactly = 1){
+        coVerify(exactly = 1) {
             dailyImageDao.geDailyImage()
         }
 
     }
+
+    @Test
+    fun `My second testing function `() = runBlocking {
+
+        val expected = listOf(AstroType(1, "star", "image_url"))
+
+        val result = astroRepositoryImpl.getTypeAstroFromDB()
+
+        coVerify(exactly = 1) {
+            astroTypeDao.geAstroTypes()
+        }
+
+    }
+
+    @Test
+    fun `My second third function `() = runBlocking {
+
+        val expected =
+            listOf(
+                AstroDetail(
+                    1,
+                    "test",
+                    1,
+                    "test body",
+                    "ttt",
+                    "descriotion",
+                    1,
+                    "img_url0",
+                    1
+                )
+            )
+
+        val result = astroRepositoryImpl.getAllAstrosDetailFromBD()
+
+        coVerify(exactly = 1) {
+            astroDetailDao.getAstrosDetail()
+        }
+
+    }
+
+
 }
